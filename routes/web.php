e1483a1ce79fe4159d2ab411241a054b968e85a9
Wildan Belfiore <mikewildan@outlook.com>
2024-03-06 16:31:01 +0700

    penambahan navigation user managemnt pada superadmin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\SuperAdmin\SuperAdminController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
